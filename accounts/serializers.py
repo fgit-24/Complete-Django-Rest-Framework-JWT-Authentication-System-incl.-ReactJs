222283adfb81beaf3493c530882ea64f268e234b
Florian Hass <f91hama@gmail.com>
2024-09-14 19:38:27 +0000

    Add RegisterUserView to views.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from .models import User
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=68, min_length=6, write_only=True)
+    password2=serializers.CharField(max_length=68, min_length=6, write_only=True)
+
+    class Meta:
+        model=User
+        fields=['email', 'first_name', 'last_name', 'password', 'password2']
 
 
+    def validate(self, attrs):
+
+        return super().validate(attrs)
 
 
+    def create(self, validated_data):
 
-
+        return super().create(validated_data)
