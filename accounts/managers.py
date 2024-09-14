551e47a37e5ac3eb2ddfeb8dde9750368e2b9415
Florian Hass <f91hama@gmail.com>
2024-09-14 11:00:42 +0000

    Implement superuser creation in UserManager
    - Added functionality to create superusers in the UserManager class.
    - Updated create_user method to handle superuser-specific fields.
    - Made migrations to apply changes to the database schema.

--- accounts/managers.py
+++ accounts/managers.py
@@ -10,6 +10,7 @@ class UserManager(BaseUserManager):
         except ValidationError:
             raise ValueError(_("Please enter a valid email address!"))
 
+
     def create_user(self, email, first_name, last_name, password, **extra_fields):
         if email:
             email=self.normalize_email(email)
@@ -24,3 +25,20 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+    extra_fields.setdefault("is_staff", True)
+    extra_fields.setdefault("is_superuser", True)
+    extra_fields.setdefault("is_verified", True)
+
+    if extra_fields.get("is_staff") is not True:
+        raise ValueError (_("is_staff must be true for admin user"))
+    
+    if extra_fields.get("is_superuser") is not True:
+        raise ValueError(_("is_superuser must be true for admin user"))
+
+    user = self.create_user(
+        email, first_name, last_name, password, **extra_fields
+    )
+    user.save(using=self._db)
+    return user
--- accounts/models.py
+++ accounts/models.py
@@ -1,29 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
-from .manager.py import UserManager
+
+from accounts.managers import UserManager
+# Create your models here.
+
+AUTH_PROVIDERS ={'email':'email', 'google':'google', 'github':'github', 'linkedin':'linkedin'}
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True, verbose_name=_("Email Address"))
-    first_name = models.CharField(max_length=100, verbose_name="First Name")
-    last_name = models.CharField(max_length=100, verbose_name="Last Name")
+    id = models.BigAutoField(primary_key=True, editable=False) 
+    email = models.EmailField(
+        max_length=255, verbose_name=_("Email Address"), unique=True
+    )
+    first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=100, verbose_name=_("Last Name"))
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_verified = models.BooleanField(default=False)
+    is_verified=models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
+    auth_provider=models.CharField(max_length=50, blank=False, null=False, default=AUTH_PROVIDERS.get('email'))
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
 
-    def __str__(self):
-        return self.email
-
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}"
-
     def tokens(self):
-        pass
+        pass  
