3c5d0f9ed730f06615fc970b6a71266fdfa77e11
Florian Hass <f91hama@gmail.com>
2024-09-14 21:44:28 +0000

    Add OTP generation and email sending functionality
    - Implemented  function to generate an OTP for a user and save it to the OneTimePassword model.
    - Added functionality to send the OTP via email to the user.
    - Used Django's  for sending emails with error handling using .
    - Dynamically retrieved user's first name and created personalized email content.

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,5 +1,9 @@
 import random
 from django.core.mail import EmailMessage
+from .models import User, OneTimePassword
+from .utils import generate_opt
+
+
 
 def generate_opt():
     """
@@ -12,3 +16,15 @@ def generate_opt():
 
 
 def send_code_to_user(email):
+    Subject='One time passcode for email verification'
+    opt_code=generateOpt()
+    print(opt_code)
+    user=User.objects.get(email=email)
+    current_site="myAuth.com"
+    email_body=f"Hi {user.first_name} thanks for signing up on {current_site} please verify your email with the \n one time passcode {otp_code}!"
+    from_email=settings.DEFAULT_FROM_EMAIL
+
+    OneTimePassword.objects.create(user=user, code=opt_code)
+
+    d_email=EmailMessage(subject=Subject, body=email_body, from_email=from_email, to=[email])
+    d_email.send(fail_silently=True)
